51c8b9f0c19611a4393b41663e4541d6a45c9687
Henrik Larsen Toft <henrik@Henriks-MacBook-Pro.local>
2018-12-15 11:30:56 +0100

    cleanup

--- luke15/index.js
+++ luke15/index.js
@@ -6,25 +6,19 @@ let input = fs.readFileSync('./input.txt', 'utf-8').split('\n').map(p => (
 
 input = input.slice(0, input.length-1);
 
-const hasGoldBirthdate = (f) => {
-  let age = Math.floor(Math.sqrt(f));
-  let sq = Math.sqrt(f + age);
-
-  while (sq >= age) {
-    if (age === sq) return true;
-    age++;
-    sq = Math.sqrt(f + age);
-  }
-
-  return false;
-}
-
 const solve = (arr) => {
   let counter = 0;
-  arr.forEach((y) => {
-    counter += hasGoldBirthdate(y) ? 1 : 0;
+  arr.forEach((f) => {
+    let age = Math.floor(Math.sqrt(f));
+    let sq = Math.sqrt(f + age);
+
+    while (sq >= age) {
+      if (age === sq) return ++counter;
+      age++;
+      sq = Math.sqrt(f + age);
+    }
   });
   return counter;
 }
 
-console.log(solve(input));
+console.log('RESULT', solve(input));
